afc4fdd3359e73c25752909b7b2dee3e4907e751
TheStrawberry-09 <sunsun12217@gmail.com>
2025-10-24 20:22:31 +0700

    Update firebase.js
    remove key
    re get in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import "firebase/firestore";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyBKZ9qbKo4YRtjMwvK4LcCF54-m_ZMavSc",
-  authDomain: "japanese-word-battle.firebaseapp.com",
-  databaseURL:
-    "https://japanese-word-battle-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "japanese-word-battle",
-  storageBucket: "japanese-word-battle.appspot.com",
-  messagingSenderId: "936660245889",
-  appId: "1:936660245889:web:968a958904de9332609c80",
-  measurementId: "G-DYTEZJ7XTH",
 };
 
 // Initialize Firebase
